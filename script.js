e7587b28f55a895e6c4706684db0be1536de1ef7
Matheus Amorim <mtxdevfrontend@gmail.com>
2023-10-18 13:19:54 -0300

    Melhoria do código

--- script.js
+++ script.js
@@ -1,83 +1,64 @@
-let adi = document.querySelector(`button#add`)
-adi.addEventListener(`click`, adicionar)
-let txt = document.querySelector(`input#txt`)
-let taskList = document.querySelector(`ul#task-list`)
-let lista = []
+let adi = document.querySelector(`button#add`);
+adi.addEventListener(`click`, adicionar);
+let txt = document.querySelector(`input#txt`);
+let taskList = document.querySelector(`ul#task-list`);
+let lista = [];
 
+const isText = (tex) => tex.length > 0;
 
+const inList = (texval, list) => list.find((texval2) => texval2.comando === texval);
 
-function isText(t){
-    if(t.length > 0){
-        return true
-    } else{
-        return false
-    }
+function adicionar() {
+  if (isText(txt.value) && inList(txt.value, lista) == undefined) {
+    lista.push({
+      comando: txt.value,
+      concluida: false,
+    });
+
+    txt.value = ``;
+    txt.focus();
+
+    mostrar();
+  } else {alert(`Tarefa inválida ou ja adicionada!`)};
 }
 
-function inList(t, l){
-    if(l.indexOf(t) != -1){
-        return true
-    } else {
-        return false
-    }
-}
+function mostrar() {
+  let item = ``;
 
-
-function adicionar(){
-    if(isText(txt.value) && !inList(txt.value, lista)){
-        lista.push({
-            comando: txt.value,
-            concluida: false
-        })
-
-        txt.value = ``
-        txt.focus()
-
-        mostrar()
-    } else[
-        alert(`Tarefa inválida ou ja adicionada!`)
-    ]
-    
-}
-
-function mostrar(){
-    let item = ``
-    
-    lista.forEach((tarefa, index) =>{  //foreach pega cada valor do array
-        item = item += `
+  lista.forEach((tarefa, index) => {
+    //foreach pega cada valor do array
+    item = item += `
         <li class="task ${tarefa.concluida && "feito"}">
             <img src="./imagens/checked.png" alt="check" onclick="checar(${index})">
             <p>${tarefa.comando}</p>
             <img src="./imagens/trash.png" alt="remover" onclick="deletar(${index})">
         </li>
-        `
-    })
+        `;
+  });
 
-    taskList.innerHTML = item
-    localStorage.setItem(`lista`, JSON.stringify(lista) )
-
+  taskList.innerHTML = item;
+  localStorage.setItem(`lista`, JSON.stringify(lista));
 }
 
-function deletar(index){
-    lista.splice(index, 1)
-    
-    mostrar()
+const deletar = (index) => {
+  lista.splice(index, 1);
+  mostrar();
+};
+
+const checar = (index) => {
+  lista[index].concluida = !lista[index].concluida; //inversao de valor usando !
+
+  mostrar();
 }
 
-function checar(index){
-    lista[index].concluida = !lista[index].concluida //inversao de valor usando !
+const recarregar = () => {
+  let reload = localStorage.getItem(`lista`);
 
-    mostrar()
+  if (reload) {
+    lista = JSON.parse(reload);
+  }
+
+  mostrar();
 }
 
-function recarregar(){
-    let reload = localStorage.getItem(`lista`)
-
-    if(reload){
-        lista = JSON.parse(reload)
-    }
-
-    mostrar()
-}
-
-recarregar()
+recarregar();
